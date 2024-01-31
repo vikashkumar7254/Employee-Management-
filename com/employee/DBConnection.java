a65c077c46aa95f22460ac0869926baeb27645dd
Vikash kumar <vks725482@gmail.com>
2024-01-31 15:31:05 +0530

    files upload

---
+++ com/employee/DBConnection.java
@@ -0,0 +1,15 @@
+package com.employee;
+
+import java.sql.Connection;
+
+public class DBConnection {
+    static Connection con;
+    public static Connection CreateDBConnection(){
+        try {
+            //
+        }catch (Exception e){
+
+        }
+        return con;
+    }
+}
