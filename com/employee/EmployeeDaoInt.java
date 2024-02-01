fce1384594c987fbb9faabcc9f2e8e9ec3b98773
pankaj881688 <pankaj881688@gmail.com>
2024-02-01 14:04:06 +0530

    first commit

--- com/employee/EmployeeDaoInt.java
+++ com/employee/EmployeeDaoInt.java
@@ -1,4 +1,10 @@
 package com.employee;
 
 public interface EmployeeDaoInt {
+    public void create(Employee emp);
+    public void showall();
+    public void showbyid(int id);
+    public void update();
+    public void delete();
+
 }
--- com/employee/main.java
+++ com/employee/main.java
@@ -1,4 +1,85 @@
 package com.employee;
 
-public class main {
-}
+import java.util.Scanner;
+
+
+
+
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc= new Scanner(System.in);
+            EmployeeDaoInt inf= new EmployeeDaoImp();
+            Employee emp= new Employee();
+            System.out.println("Welcome to main.Employee Management System........");
+            int choice = 0;
+            System.out.println("1.Press 1 to Add new main.Employee");
+            System.out.println("2.Press 2 to Show all Employees");
+            System.out.println("3.Press 3 to Show main.Employee by id");
+            System.out.println("4.Press 4 to Update main.Employee record");
+            System.out.println("5.Press 5 to delete employee record");
+            System.out.println("6.Press 6 to Exit..");
+            System.out.println("Enter your choice..: ");
+            choice = sc.nextInt();
+            if (choice == 1) {
+                System.out.println("Enter Id :");
+                int id=sc.nextInt();
+                sc.nextLine();
+                System.out.println("Enter Name :");
+                String name=sc.nextLine();
+                System.out.println("Enter age :");
+                int age=sc.nextInt();
+                sc.nextLine();
+                System.out.println("Enter Gender :");
+                String gender=sc.nextLine();
+                System.out.println("Enter Designation :");
+                String designation=sc.nextLine();
+                System.out.println("Enter Blood Group :");
+                String blood_group=sc.nextLine();
+                System.out.println("Enter Salary :");
+                double salary=sc.nextDouble();
+                System.out.println("Enter Phone Number :");
+                long ph=sc.nextLong();
+                sc.nextLine();
+                System.out.println("Enter Email :");
+                String email=sc.nextLine();
+
+                emp.setId(id);
+                emp.setName(name);
+                emp.setAge(age);
+                emp.setGender(gender);
+                emp.setDesignation(designation);
+                emp.setBloodGp(blood_group);
+                emp.setSalary(salary);
+                emp.setPhNumber(ph);
+                emp.setEmail(email);
+                inf.create(emp);
+
+            }
+            if (choice == 2) {
+                inf.showall();
+
+            }
+            if (choice == 3) {
+                System.out.println("Enter Employee id :");
+                int id=sc.nextInt();
+
+                inf.showbyid(id);
+
+            }
+            if (choice == 4) {
+
+
+            }
+            if (choice == 5) {
+
+            }
+            if (choice == 6) {
+
+
+
+            }
+
+        }
+    }
+
