5ae91fb359d108853dd52155a617ba66801e1020
Vikash kumar <vks725482@gmail.com>
2024-02-12 15:44:12 +0530

    commit 9

--- com/employee/Authentication.java
+++ com/employee/Authentication.java
@@ -12,15 +12,18 @@ public class Authentication {
     void staffAuthenticate(){
 
         con=DBConnection.CreateDBConnection();
+
+        System.out.println("");
         System.out.println("Press 1 for SIGN UP..");
         System.out.println("Press 2 for LOGIN..");
-        System.out.println("Enter your choice");
+        System.out.print("Enter Your Choice: ");
         int ch=sc.nextInt();
         if(ch==1){
             sc.nextLine();
-            System.out.println("Enter username");
+            System.out.println("");
+            System.out.print("Enter Username: ");
             String username=sc.nextLine();
-            System.out.println("Enter password");
+            System.out.print("Enter Password: ");
             String password=sc.nextLine();
             try{
                 PreparedStatement ps = con.prepareStatement("insert into admin_login values(?,?)");
@@ -36,12 +39,13 @@ public class Authentication {
         }
         if(ch==2){
             sc.nextLine();
-            System.out.println("Enter username..");
+            System.out.println("");
+            System.out.print("Enter Username: ");
             String username=sc.nextLine();
-            System.out.println("Enter password");
+            System.out.print("Enter Password: ");
             String password=sc.nextLine();
             try {
-                PreparedStatement ps =con.prepareStatement("select *from admin_login where Username=? and Password_=?");
+                PreparedStatement ps =con.prepareStatement("select * from admin_login where Username=? and Password_=?");
                 ps.setString(1,username);
                 ps.setString(2,password);
                 ResultSet rs=ps.executeQuery();
@@ -59,21 +63,28 @@ public class Authentication {
         }
 
     }
-    void employeeAuthenticate(){
+    void employeeAuthenticate()
+    {
         con=DBConnection.CreateDBConnection();
-        System.out.println("Enter your ID to LOGIN..");
+        System.out.println("");
+        System.out.print("Enter your ID to LOGIN: ");
         int id=sc.nextInt();
-        try {
-                PreparedStatement ps = con.prepareStatement("select *from employee where Emp_id=?");
+        try
+        {
+                PreparedStatement ps = con.prepareStatement("select * from employee where Emp_id=?");
                 ps.setInt(1,id);
                 ResultSet rs= ps.executeQuery();
-                if(rs.next()){
+                if(rs.next())
+                {
                         m1.emp_Menu(id);
                 }
-                else{
-                    System.out.println("you have enter wrong id");
+                else
+                {
+                    System.out.println("You Have Entered Wrong ID...");
                 }
-        }catch (Exception e){
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
 
--- com/employee/Menu.java
+++ com/employee/Menu.java
@@ -7,7 +7,8 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class Menu {
+public class Menu
+{
     Connection con;
     Scanner sc= new Scanner(System.in);
     EmployeeDaoInt inf = new EmployeeDaoImp();
@@ -15,17 +16,24 @@ public class Menu {
     int levs;
     int data;
     int exit=7;
-    void Adminmenu() {
-        do {
-            System.out.println("Welcome to main.Employee Management System........");
-            System.out.println("1.Press 1 to Add new Employee");
-            System.out.println("2.Press 2 to Show all Employees");
-            System.out.println("3.Press 3 to Show main.Employee by id");
-            System.out.println("4.Press 4 to Update main.Employee record");
-            System.out.println("5.Press 5 to delete employee record");
-            System.out.println("6.Press 6 for Check Leave Request");
-            System.out.println("7.Press 7 to Exit..");
-            System.out.println("Enter your choice..: ");
+    void Adminmenu()
+    {
+        main m1 = new main();
+        do
+        {
+            System.out.println("");
+            System.out.println("------------------------------------------");
+            System.out.println("Welcome to ADMIN MENU");
+            System.out.println("------------------------------------------");
+            System.out.println("1) Press 1 to Add New Employee");
+            System.out.println("2) Press 2 to Show All Employees");
+            System.out.println("3) Press 3 to Show Employee by ID");
+            System.out.println("4) Press 4 to Update Employee Record");
+            System.out.println("5) Press 5 to Delete Employee Record");
+            System.out.println("6) Press 6 to Check Leave Request");
+            System.out.println("7) Press 7 to EXIT..");
+            System.out.println("");
+            System.out.print("Enter your Choice: ");
             int choice1 = sc.nextInt();
             switch (choice1) {
                 case 1:
@@ -86,10 +94,12 @@ public class Menu {
 
                     break;
                 case 6:
-                    System.out.println("1 show all Leave Requests");
+                {
+                    System.out.println("1 Show All Leave Requests");
                     System.out.println("2. Update Request by ID : ");
                     int lev = sc.nextInt();
-                    switch (lev) {
+                    switch (lev)
+                    {
                         case 1:
                             con = DBConnection.CreateDBConnection();
                             try {
@@ -107,36 +117,36 @@ public class Menu {
                             int ids = sc.nextInt();
 
                             System.out.println("1. For Approve\n 2. FOr or Not");
-                            int ap= sc.nextInt();
-                            if (ap==1)
+                            int ap = sc.nextInt();
+                            if (ap == 1)
                                 System.out.println("Count Total leaves Date !");
-                            levs=sc.nextInt();
+                            levs = sc.nextInt();
 
-                                try {
-                                    con = DBConnection.CreateDBConnection();
-                                    //PreparedStatement ps = con.prepareStatement("update employee set Total_leaves_available=? where Emp_id=?");
-                                    PreparedStatement ps = con.prepareStatement("select Total_leaves_available from employee where Emp_id="+ids);
-                                    ResultSet rs=ps.executeQuery();
-                                    while (rs.next()){
-                                        data=rs.getInt(1);
-                                    }
-
-
-                                } catch (Exception ex) {
-                                    ex.printStackTrace();
+                            try {
+                                con = DBConnection.CreateDBConnection();
+                                //PreparedStatement ps = con.prepareStatement("update employee set Total_leaves_available=? where Emp_id=?");
+                                PreparedStatement ps = con.prepareStatement("select Total_leaves_available from employee where Emp_id=" + ids);
+                                ResultSet rs = ps.executeQuery();
+                                while (rs.next()) {
+                                    data = rs.getInt(1);
                                 }
-                                try {
-                                    int levs1=data-levs;
-                                    con = DBConnection.CreateDBConnection();
-                                    PreparedStatement ps = con.prepareStatement("update employee set Total_leaves_available=? where Emp_id="+ids);
-                                    ps.setInt(1,levs1);
-                                    int rs = ps.executeUpdate();
-                                    if (rs != 0) {
-                                        System.out.println("Update Successfully");
-                                    }
-                                } catch (Exception ex) {
-                                    ex.printStackTrace();
+
+
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                            try {
+                                int levs1 = data - levs;
+                                con = DBConnection.CreateDBConnection();
+                                PreparedStatement ps = con.prepareStatement("update employee set Total_leaves_available=? where Emp_id=" + ids);
+                                ps.setInt(1, levs1);
+                                int rs = ps.executeUpdate();
+                                if (rs != 0) {
+                                    System.out.println("Update Successfully");
                                 }
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
                             try {
 
                                 String str = "Approve";
@@ -151,7 +161,7 @@ public class Menu {
                             } catch (Exception ex) {
                                 ex.printStackTrace();
                             }
-                            if (ap==2)
+                            if (ap == 2)
                                 try {
 
                                     String str = "Rejected";
@@ -172,72 +182,100 @@ public class Menu {
                             System.out.println("Wrong Input ! ");
                     }
 
-            break;
-            default :
-                System.out.println("Thakyou For Using application....good by");
-                break;
+                    break;
+                }
+                case 7:
+                {
+
+                    m1.MainMenu();
+                }
+                default :
+                    System.out.println("Thank You For Using The Application....");
+                    break;
+            }
+
+        }
+        while (true);
+    }
+
+    void emp_Menu(int id)
+    {
+        con=DBConnection.CreateDBConnection();
+
+        System.out.println("\n----------------------------");
+        System.out.println("Welcome to EMPLOYEE PORTAL");
+        System.out.println("----------------------------");
+        System.out.println("1) Apply For Leave");
+        System.out.println("2) Show Leave Status");
+        System.out.println("");
+        int ch=sc.nextInt();
+        if(ch==1)
+        {
+            int id1=id;
+            sc.nextLine();
+            System.out.print("Enter Name: ");
+            String name=sc.nextLine();
+            System.out.print("Enter Start to End Date");
+            String date=sc.nextLine();
+            try
+            {
+                con = DBConnection.CreateDBConnection();
+                PreparedStatement ps = con.prepareStatement("update employee set Total_leaves_available=? where Emp_id=?");
+                ps.setInt(1,levs);
+                ps.setInt(2, id);
+                int rs = ps.executeUpdate();
+                if (rs != 0)
+                {
+                    System.out.println("Record Updated Successfully...");
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+            try
+            {
+                    PreparedStatement ps= con.prepareStatement("insert into leave_db values(?,?,?,?)");
+                    ps.setInt(1,id1);
+                    ps.setString(2,name);
+                    ps.setString(3,date);
+                    ps.setString(4,"");
+                    int rs=ps.executeUpdate();
+                    if(rs!=0)
+                    {
+                        System.out.println("Leave submitted Successfully");
+                    }
+
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
         }
 
-        }while (true);
-}
-void emp_Menu(int id){
-        con=DBConnection.CreateDBConnection();
-    System.out.println("Welcome to Employeee portal");
-    System.out.println("1.Apply For Leave");
-    System.out.println("2.Show leave Status");
-    int ch=sc.nextInt();
-    if(ch==1){
-        int id1=id;
-        sc.nextLine();
-        System.out.println("Enter name :");
-        String name=sc.nextLine();
-        System.out.println("Enter Start to End date");
-        String date=sc.nextLine();
-        try {
-            con = DBConnection.CreateDBConnection();
-            PreparedStatement ps = con.prepareStatement("update employee set Total_leaves_available=? where Emp_id=?");
-           ps.setInt(1,levs);
-            ps.setInt(2, id);
-            int rs = ps.executeUpdate();
-            if (rs != 0) {
-                System.out.println("Update Successfully");
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        try{
-                PreparedStatement ps= con.prepareStatement("insert into leave_db values(?,?,?,?)");
-                ps.setInt(1,id1);
-                ps.setString(2,name);
-                ps.setString(3,date);
-                ps.setString(4,"");
-                int rs=ps.executeUpdate();
-                if(rs!=0){
-                    System.out.println("Leave submitted Successfully");
+        if (ch==2)
+        {
+            con=DBConnection.CreateDBConnection();
+            String query="SELECT * FROM leave_db WHERE Emp_id =" +id;
+
+            //String query1="select * from employee where id="+id;
+            try
+            {
+                Statement stmt= con.createStatement();
+                ResultSet result= stmt.executeQuery(query);
+                while (result.next())
+                {
+                    System.out.println(result.getInt(1)+"  "+result.getString(2)+"  "+result.getString(3)+"  "+result.getString(4));
                 }
 
-        }catch (Exception e){
-            e.printStackTrace();
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+
         }
-
-    }
-if (ch==2){
-    con=DBConnection.CreateDBConnection();
-    String query="SELECT * FROM leave_db WHERE Emp_id =" +id;
-
-    //String query1="select * from employee where id="+id;
-    try {
-        Statement stmt= con.createStatement();
-        ResultSet result= stmt.executeQuery(query);
-        while (result.next()){
-            System.out.println(result.getInt(1)+"  "+result.getString(2)+"  "+result.getString(3)+"  "+result.getString(4));
-        }
-
-}catch (Exception ex){
-        ex.printStackTrace();
-    }
-
-    }
 
     }
 
